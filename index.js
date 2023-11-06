3e497590d027844ffdd44c8c345fd42bfd4d7b9d
Dev <57229351+rocketenterprise@users.noreply.github.com>
2023-11-06 10:41:14 -0500

    update index

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-function searchHotels() {
-    const location = document.getElementById('location').value;
-    const checkIn = document.getElementById('check-in').value;
-    const checkOut = document.getElementById('check-out').value;
-    const guests = document.getElementById('guests').value;
-    // Implement hotel search logic and display results in .comparison-results
-}
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.nav-menu').classList.toggle('active');
+});
